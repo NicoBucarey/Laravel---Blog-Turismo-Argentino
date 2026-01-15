9ab42c302d95da97ce1975b67302089f64131544
Nico <nicolas.bucarey@est.fi.uncoma.edu.ar>
2026-01-15 10:45:25 -0300

    Add simple HTTPS forcing

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,7 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Simple HTTPS forcing for Railway
+        \URL::forceScheme('https');
     }
    
 
