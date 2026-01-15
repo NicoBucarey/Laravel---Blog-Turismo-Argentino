c21e167df38418519a18292e2d0d42b5a01f82aa
Nico <nicolas.bucarey@est.fi.uncoma.edu.ar>
2026-01-15 09:25:18 -0300

    Fix Vite assets include in app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Blog de Turismo')</title>
-    @vite('resources/css/app.css') {{-- Asegurate de tener Tailwind cargado --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <!-- Google Fonts -->
 <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600;700&family=Open+Sans:wght@400;600&display=swap" rel="stylesheet">
 <link rel="icon" href="{{ asset('favicon.jpg') }}" type="image/png">
