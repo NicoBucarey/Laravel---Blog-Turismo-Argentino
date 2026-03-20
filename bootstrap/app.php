7bb93f714f4091ad485ee88605a715b07c5b7448
Nico <nicolas.bucarey@est.fi.uncoma.edu.ar>
2026-03-19 21:29:39 -0300

    Fix HTTPS configuration for Railway

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // Trust all proxies (needed for Railway)
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
